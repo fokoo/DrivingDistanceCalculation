d26da735a5b799819fcd9830cee75fe573c0a40f
fokoo <50514692+fokoo@users.noreply.github.com>
2024-01-21 15:10:02 +0100

    Update main.h

--- main.h
+++ main.h
@@ -10,7 +10,6 @@
 #define RESULT_COL_SIZE (NUMBER_OF_NODES+2)
 
 
-
 void execute_program(FILE *file, int matResult[][RESULT_COL_SIZE], int matDistance[][MAX_SIZE], int matVitesse[][MAX_SIZE], int matPath[], int paths_length);
 
 void read_matrix(FILE *file, const size_t taille, int mat[][MAX_SIZE]);
@@ -22,7 +21,6 @@ void generate_paths(int paths_length, int matPath[]);
 void berechnung_laenge_fahrzeit(int matResult[][RESULT_COL_SIZE], int matPath[], int matDistance[][MAX_SIZE], int matVitesse[][MAX_SIZE], int paths_length);
 
 void show(int matResult[][RESULT_COL_SIZE], int paths_length);
-
 
 
 int number_of_path(int num_nodes);
